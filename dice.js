decea6a05f0c0fb5b1d1a713ae6853b3b48484dc
tobiloba7 <91735098+tobiloba7@users.noreply.github.com>
2022-04-25 10:49:45 +0100

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,19 @@
+let randomNumber1 = Math.random();
+randomNumber1 = Math.floor(randomNumber1 * 6) + 1
+
+let randomNumber2 = Math.random();
+randomNumber2 = Math.floor(randomNumber2 * 6) + 1
+
+
+
+document.querySelector('.img1').setAttribute('src', 'images/dice' +  randomNumber1 + '.png')
+document.querySelector('.img2').setAttribute('src', 'images/dice' +  randomNumber2 + '.png')
+
+
+if (randomNumber1 > randomNumber2 ) {
+    document.querySelector('h1').innerHTML = 'Player 1 wins!'
+} else if (randomNumber2 > randomNumber1 ) {
+    document.querySelector('h1').innerHTML = 'Player 2 wins!'
+} else {
+    document.querySelector('h1').innerHTML = 'Draw!'
+}
